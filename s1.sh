29550b6afc266019b401599c2bdd1aaf9bb2bf24
camilacm <ccmiranda@ciencias.unam.mx>
2018-07-24 12:55:15 -0500

    actividad en proceso tipo shell

---
+++ s1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Inicia el script 1"
+ls
+mkdir magia
+touch magia/alibaba
+echo "Hocus pocus" > magia/alibaba
+cat magia/alibaba
+echo "Fin del script 1"
---
+++ s2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Inicia el scrip 2"
+./s1.sh
+./s2.sh arch1 dos
+echo $1
+echo $2
+touch $1
+ls
+mvdir
